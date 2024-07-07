56f48bdaf8144ed45f76c588639b04f4802338f1
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-07-07 17:33:30 +0900

    :tada:

---
+++ src/main/kotlin/me/bossm0n5t3r/KotlinSpringBootPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package me.bossm0n5t3r
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootPlaygroundApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringBootPlaygroundApplication>(*args)
+}
