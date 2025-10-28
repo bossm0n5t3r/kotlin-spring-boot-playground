bd61d5e99de76af9621ae6e51d9136eeb59af6cd
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-10-29 00:29:00 +0900

    feat: add kotlinx-coroutines-reactor dependency
    - Introduced `kotlinx-coroutines-reactor` for reactive programming with coroutines
    - Added version reference for `kotlinx-coroutines` in `libs.versions.toml`

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ repositories {
 
 dependencies {
     implementation(libs.kotlin.reflect)
+    implementation(libs.kotlinx.coroutines.reactor)
+
     implementation(libs.spring.boot.starter.data.jdbc)
     implementation(libs.spring.boot.starter.web)
     implementation(libs.spring.boot.starter.webflux)
