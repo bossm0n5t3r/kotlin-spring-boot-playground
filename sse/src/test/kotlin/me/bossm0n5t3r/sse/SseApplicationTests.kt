372640a02095f3a746681d1c5859e82f6d36da25
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-11-29 00:03:00 +0900

    test(sse): add context load test for SseApplication
    - Created `SseApplicationTests` with a basic `contextLoads` test

---
+++ sse/src/test/kotlin/me/bossm0n5t3r/sse/SseApplicationTests.kt
@@ -0,0 +1,11 @@
+package me.bossm0n5t3r.sse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SseApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
