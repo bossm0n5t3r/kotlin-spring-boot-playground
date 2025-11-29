45513a549b4ac39f09ae2ec1390548f2d4e94c8f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-11-30 03:50:00 +0900

    refactor(sse): rename SseController to RedisSseController
    - Update request mapping to `/sse/redis`
    - Inject `redisStreamEventStore` with `@Qualifier`

--- sse/src/main/kotlin/me/bossm0n5t3r/sse/controller/RedisSseController.kt
+++ sse/src/main/kotlin/me/bossm0n5t3r/sse/controller/RedisSseController.kt
@@ -1,7 +1,9 @@
 package me.bossm0n5t3r.sse.controller
 
+import me.bossm0n5t3r.sse.constant.Constants.LAST_EVENT_ID
 import me.bossm0n5t3r.sse.dto.SseEvent
 import me.bossm0n5t3r.sse.service.EventStore
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.MediaType
 import org.springframework.http.codec.ServerSentEvent
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,14 +14,11 @@ import reactor.core.publisher.Flux
 import java.time.Duration
 
 @RestController
-@RequestMapping("/sse")
-class SseController(
+@RequestMapping("/sse/redis")
+class RedisSseController(
+    @param:Qualifier("redisStreamEventStore")
     private val eventStore: EventStore,
 ) {
-    companion object {
-        private const val LAST_EVENT_ID = "Last-Event-ID"
-    }
-
     @GetMapping(produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
     fun sse(
         @RequestHeader(LAST_EVENT_ID, required = false)
