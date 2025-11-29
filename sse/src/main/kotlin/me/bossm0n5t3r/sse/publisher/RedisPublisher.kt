52ae9e4395472d3c43ba8f78056d4b69091c0ea8
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-11-30 08:10:00 +0900

    refactor(sse): remove @EnableScheduling from RedisPublisher
    - Remove unnecessary `@EnableScheduling` annotation
    - Update logging for Redis Stream SSE events

--- sse/src/main/kotlin/me/bossm0n5t3r/sse/publisher/RedisPublisher.kt
+++ sse/src/main/kotlin/me/bossm0n5t3r/sse/publisher/RedisPublisher.kt
@@ -3,14 +3,12 @@ package me.bossm0n5t3r.sse.publisher
 import me.bossm0n5t3r.sse.configuration.LOGGER
 import me.bossm0n5t3r.sse.service.EventStore
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import kotlin.time.Clock
 import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
-@EnableScheduling
 @Component
 class RedisPublisher(
     @param:Qualifier("redisStreamEventStore")
@@ -18,7 +16,7 @@ class RedisPublisher(
 ) : EventPublisher {
     @Scheduled(fixedRate = 5_000)
     override fun publish() {
-        val event = eventStore.publish("tick at ${Clock.System.now()}")
-        LOGGER.info("published event: {}", event)
+        val event = eventStore.publish("[redis] tick at ${Clock.System.now()}")
+        LOGGER.info("published Redis Stream SSE event: {}", event)
     }
 }
