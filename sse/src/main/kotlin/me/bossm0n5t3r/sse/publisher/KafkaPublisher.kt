b52237826f4bd938537b932165d435603dbca18e
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-11-30 08:27:00 +0900

    feat(sse): add KafkaPublisher for scheduled event publishing
    - Implement `KafkaPublisher` to publish SSE events periodically
    - Use `@Scheduled` to trigger event publishing every 5 seconds

---
+++ sse/src/main/kotlin/me/bossm0n5t3r/sse/publisher/KafkaPublisher.kt
@@ -0,0 +1,22 @@
+package me.bossm0n5t3r.sse.publisher
+
+import me.bossm0n5t3r.sse.configuration.LOGGER
+import me.bossm0n5t3r.sse.service.EventStore
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+
+@OptIn(ExperimentalTime::class)
+@Component
+class KafkaPublisher(
+    @param:Qualifier("kafkaEventStore")
+    override val eventStore: EventStore,
+) : EventPublisher {
+    @Scheduled(fixedRate = 5000)
+    override fun publish() {
+        val event = eventStore.publish("[kafka] tick at ${Clock.System.now()}")
+        LOGGER.info("published Kafka SSE event: {}", event)
+    }
+}
