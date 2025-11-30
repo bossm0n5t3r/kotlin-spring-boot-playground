2239d68771b1c666b352ea332349afa8eda56002
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-11-30 09:03:00 +0900

    feat(sse): add string-to-SseEvent conversion utility
    - Implement `String.toSseEvent` to map JSON strings to `SseEvent` objects
    - Leverage `kotlinx.serialization` for JSON parsing

--- sse/src/main/kotlin/me/bossm0n5t3r/sse/dto/SseEvent.kt
+++ sse/src/main/kotlin/me/bossm0n5t3r/sse/dto/SseEvent.kt
@@ -1,6 +1,9 @@
 package me.bossm0n5t3r.sse.dto
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import me.bossm0n5t3r.sse.utility.JsonUtil.JSON
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
@@ -12,3 +15,19 @@ data class SseEvent(
     val message: String,
     val createdAt: Instant,
 )
+
+@OptIn(ExperimentalTime::class)
+fun String.toSseEvent(offset: String): SseEvent {
+    val node = JSON.decodeFromString<JsonObject>(this)
+
+    val ulid = node["ulid"]?.jsonPrimitive?.content.toString()
+    val message = node["message"]?.jsonPrimitive?.content.toString()
+    val createdAt = Instant.parse(node["createdAt"]?.jsonPrimitive?.content.toString())
+
+    return SseEvent(
+        streamId = offset,
+        ulid = ulid,
+        message = message,
+        createdAt = createdAt,
+    )
+}
