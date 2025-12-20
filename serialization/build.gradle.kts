e9c91d8aa4b12896c57f5f75496d1ad36e304e2a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-12-21 04:30:00 +0900

    feat(gradle): configure serialization module build script
    - Add Kotlin JVM, serialization, and Ktlint plugins.
    - Set up dependencies for JSON serialization and JUnit5 testing.
    - Define toolchain, repositories, and compiler options.
    - Configure Ktlint version.

--- serialization/build.gradle.kts
+++ serialization/build.gradle.kts
@@ -1,1 +1,51 @@
-dependencies {}
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.plugin.serialization)
+    alias(libs.plugins.ktlint)
+}
+
+group = "me.bossm0n5t3r"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion =
+            JavaLanguageVersion.of(
+                libs.versions.jdk
+                    .get()
+                    .toInt(),
+            )
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.tools.jackson.module.kotlin)
+
+    testImplementation(libs.kotlinx.serialization.json)
+    testImplementation(libs.kotlinx.serialization.jvm.extra)
+
+    testImplementation(libs.gson)
+
+    testImplementation(libs.kotlin.test.junit5)
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+ktlint {
+    version.set(
+        libs.versions.pinterest.ktlint
+            .get(),
+    )
+}
