a04ac2f9565b19f03d1c23c79c2724a57aea3146
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-12-27 05:00:00 +0900

    feat(gradle): configure `content-negotiation` module with dependencies and plugins
    - Add necessary plugins: Kotlin, Spring Boot, ktlint, and Protobuf.
    - Configure dependencies for Spring, Jackson, and Protobuf.
    - Set up Kotlin compiler options, testing with JUnit, and ktlint rules.
    - Integrate Protobuf plugin and configure tasks.

--- content-negotiation/build.gradle.kts
+++ content-negotiation/build.gradle.kts
@@ -1,1 +1,74 @@
-dependencies {}
+import com.google.protobuf.gradle.id
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.plugin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.protobuf)
+}
+
+group = "me.bossm0n5t3r"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion =
+            JavaLanguageVersion.of(
+                libs.versions.jdk
+                    .get()
+                    .toInt(),
+            )
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.kotlin.reflect)
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.jackson.dataformat.msgpack)
+    implementation(libs.protobuf.java)
+    implementation(libs.protobuf.kotlin)
+
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.kotlin.test.junit5)
+    testRuntimeOnly(libs.junit.platform.launcher)
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+ktlint {
+    version.set(
+        libs.versions.pinterest.ktlint
+            .get(),
+    )
+    filter {
+        exclude { it.file.path.contains("/build/") }
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:${libs.versions.google.protobuf.get()}"
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                id("kotlin")
+            }
+        }
+    }
+}
