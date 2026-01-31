9835726c5ac30440c7cabf05dbcd78c76bfd1952
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-01-31 21:00:00 +0900

    feat(mvc): add `security-mvc` module
    - Create `security-mvc` module with Gradle configuration.
    - Add dependencies for Kotlin, Spring Boot, and testing libraries.
    - Include `security-mvc` in project settings.

---
+++ security-mvc/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.plugin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.ktlint)
+}
+
+group = "me.bossm0n5t3r"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion =
+            JavaLanguageVersion.of(
+                libs.versions.jdk
+                    .get()
+                    .toInt(),
+            )
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlin.reflect)
+
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.aop)
+    implementation(libs.jackson.module.kotlin)
+
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlin.test.junit5)
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+ktlint {
+    version.set(
+        libs.versions.pinterest.ktlint
+            .get(),
+    )
+}
