3c03979a3b130ad629fd00e4b0be9638cc521217
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-02-02 00:09:00 +0900

    refactor(mvc): replace `UserContext` with `SecurityContextHolder` in `UserServiceTest`
    - Update tests to use `SecurityContextHolder` for user authentication setup.
    - Adjust `UserService` to retrieve current user from `SecurityContextHolder`.

--- security-mvc/src/main/kotlin/me/bossm0n5t3r/security/mvc/service/UserService.kt
+++ security-mvc/src/main/kotlin/me/bossm0n5t3r/security/mvc/service/UserService.kt
@@ -1,10 +1,10 @@
 package me.bossm0n5t3r.security.mvc.service
 
-import me.bossm0n5t3r.security.mvc.context.UserContext
 import me.bossm0n5t3r.security.mvc.dto.UserDetail
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 
 @Service
 class UserService {
-    fun getCurrentUser(): UserDetail? = UserContext.getUserDetail()
+    fun getCurrentUser(): UserDetail? = SecurityContextHolder.getContext().authentication?.principal as? UserDetail
 }
--- security-mvc/src/test/kotlin/me/bossm0n5t3r/security/mvc/service/UserServiceTest.kt
+++ security-mvc/src/test/kotlin/me/bossm0n5t3r/security/mvc/service/UserServiceTest.kt
@@ -1,18 +1,19 @@
 package me.bossm0n5t3r.security.mvc.service
 
-import me.bossm0n5t3r.security.mvc.context.UserContext
 import me.bossm0n5t3r.security.mvc.dto.UserDetail
 import me.bossm0n5t3r.security.mvc.enumeration.UserRole
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
 
 class UserServiceTest {
     private val userService = UserService()
 
     @AfterEach
     fun tearDown() {
-        UserContext.clear()
+        SecurityContextHolder.clearContext()
     }
 
     @Test
@@ -25,7 +26,7 @@ class UserServiceTest {
                 email = "test@example.com",
                 roles = listOf(UserRole.USER),
             )
-        UserContext.setUserDetail(user)
+        SecurityContextHolder.getContext().authentication = UsernamePasswordAuthenticationToken(user, null, user.authorities)
 
         val currentUser = userService.getCurrentUser()
         assertEquals(user, currentUser)
