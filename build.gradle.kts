2b316ddb11eb577b745b4bb2691b54e79f387d39
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-02-06 06:00:00 +0900

    build: add plugin aliases to `build.gradle.kts`
    - Configure Kotlin and Spring Boot plugins as aliases without applying them.

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.plugin.spring) apply false
+    alias(libs.plugins.kotlin.plugin.jpa) apply false
+    alias(libs.plugins.kotlin.plugin.allopen) apply false
+    alias(libs.plugins.spring.boot) apply false
+    alias(libs.plugins.spring.dependency.management) apply false
+}
