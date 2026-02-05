3c513a5f39d844d675ac5d08fa1d2f9b219c678a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-02-06 06:11:00 +0900

    feat(tx-routing-datasource): add global exception handling for `NotFoundException`
    - Implement `ExceptionHandler` to handle `NotFoundException` with appropriate HTTP response.
    - Add `NotFoundException` class to define custom exception logic.

---
+++ tx-routing-datasource/src/main/kotlin/me/bossm0n5t3r/txroutingdatasource/exception/ExceptionHandler.kt
@@ -0,0 +1,13 @@
+package me.bossm0n5t3r.txroutingdatasource.exception
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class ExceptionHandler {
+    @ExceptionHandler(NotFoundException::class)
+    @ResponseBody
+    fun handleApiException(e: NotFoundException): ResponseEntity<String> = ResponseEntity(e.message, e.httpStatus)
+}
